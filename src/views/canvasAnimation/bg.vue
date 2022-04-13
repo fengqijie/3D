846a5cc81297d585f503a29d1577d0146ac5335e
fengqijie <1214143356@qq.com>
2022-04-13 16:52:57 +0800

    修改readme

--- src/views/canvasAnimation/bg.vue
+++ src/views/canvasAnimation/bg.vue
@@ -19,6 +19,9 @@
       class="cash"
     >
     </vue-particles>
+    <div class="text">
+      <canvas id="content"></canvas>
+    </div>
   </div>
 </template>
 
@@ -27,7 +30,36 @@ import Vue from "vue";
 import VueParticles from "vue-particles";
 Vue.use(VueParticles);
 
-export default {};
+export default {
+  data() {
+    return {
+      ctx: ""
+    };
+  },
+  mounted() {
+    this.initContent();
+  },
+  methods: {
+    initContent() {
+      // 初始化canvas
+      var ctx = document.getElementById("content");
+      ctx = ctx.getContext("2d");
+      // this.ctx = ctx;
+      ctx.save();
+      ctx.beginPath();
+      ctx.font = "40px 娃娃体-简";
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+      ctx.lineWidth = 3;
+      ctx.strokeStyle = "white";
+      ctx.strokeText("canvas 入门", 130, 40);
+      ctx.fillStyle = "pink"; //设置填充颜色
+      ctx.fillText("canvas 入门", 130, 40);
+      ctx.closePath();
+      ctx.restore();
+    }
+  }
+};
 </script>
 
 <style lang="less" scoped>
@@ -39,5 +71,9 @@ export default {};
   height: 100%;
   overflow: hidden;
   background: linear-gradient(to bottom, #dcdcdc 0%, palevioletred 100%);
+}
+.text {
+  position: absolute;
+  z-index: 100;
 }
 </style>
